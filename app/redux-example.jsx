0b60784fc4c93652a307640811f7d6cf833c26d2
Fredy Rosales <fredyr78@yahoo.com>
2016-11-29 11:45:42 -0800

    Add support for CHANGE_NAME action

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -4,9 +4,25 @@
 
   var reducer = (state = {name: 'Anonymous'}, action) => {
     // state = state || {name: 'Anonymous'}; es5 way
-    return state;
+    
+    switch (action.type) {
+      case 'CHANGE_NAME':
+        return {
+          ...state,
+          name: action.name
+        };
+      default:
+        return state;
+    }
   };
   var store = redux.createStore(reducer);
 
   var currentState = store.getState();
   console.log('currentState', currentState);
+
+  store.dispatch({
+    type: 'CHANGE_NAME',
+    name: 'Fredy'
+  });
+
+  console.log('Name Should be Fredy', store.getState());
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -1,15 +1,29 @@
   var redux = require('redux');
 
-  console.log('Starting todo-example');
-
   var stateDefault = {
     searchText: '',
     showCompleted: false,
     todos: []
   };
   var reducer = (state = stateDefault, action) => {
-    return state;
+
+    switch (action.type) {
+      case 'CHANGE_SEARCH_TEXT':
+        return {
+          ...state,
+          searchText: action.searchText
+        };
+      default:
+        return state;
+    }
   };
   var store = redux.createStore(reducer);
 
   console.log('currentState', store.getState());
+
+  store.dispatch({
+    type: 'CHANGE_SEARCH_TEXT',
+    searchText: 'Have Fun'
+  });
+
+  console.log('Text should be Have fun', store.getState());
