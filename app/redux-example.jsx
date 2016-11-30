0ca7101071cdbe9f6d2addd6c4d664da1a2541ab
Fredy Rosales <fredyr78@yahoo.com>
2016-11-30 13:11:44 -0800

    Add support for location

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,4 +1,5 @@
   var redux = require('redux');
+  var axios = require('axios');
 
   console.log('starting redux example');
 
@@ -90,12 +91,55 @@
     };
   };
 
+  // Map Reducer and action Generators
   // --------------------------------
+
+  var mapReducer = (state = {isFetching: false, url: undefined}, action) => {
+    switch (action.type) {
+      case 'START_LOCATION_FETCH':
+        return {
+          isFetching: true,
+          url: undefined
+        };
+      case 'COMPLETE_LOCATION_FETCH':
+        return {
+          isFetching: false,
+          url: action.url
+        };
+      default:
+        return state;
+    }
+  };
+
+  var startLocationFetch = () => {
+    return {
+      type: 'START_LOCATION_FETCH'
+    };
+  };
+
+  var completeLocationFetch = (url) => {
+    return {
+      type: 'COMPLETE_LOCATION_FETCH',
+      url
+    };
+  };
+
+  var fetchLocation = () => {
+    store.dispatch(startLocationFetch());
+
+    axios.get('http://ipinfo.io').then(function (res) {
+      var loc = res.data.loc;
+      var baseUrl = 'http://maps.google.com?q='
+
+      store.dispatch(completeLocationFetch(baseUrl + loc));
+    });
+  };
 
   var reducer = redux.combineReducers({
     name: nameReducer,
     hobbies: hobbiesReducer,
-    movies: moviesReducer
+    movies: moviesReducer,
+    map: mapReducer
   });
 
   var store = redux.createStore(reducer, redux.compose(
@@ -106,15 +150,20 @@
   var unsubscribe = store.subscribe(() => {
     var state = store.getState();
 
-    console.log('Name is', state.name);
-    document.getElementById('app').innerHTML = state.name;
-
     console.log('New state', store.getState());
+
+    if (state.map.isFetching) {
+      document.getElementById('app').innerHTML = 'Loading...';
+    } else if (state.map.url) {
+      document.getElementById('app').innnerHTML = '<a href="' + state.map.url + '" target="_blank">View your location</a>';
+    }
   });
 
+  //unsubscribe();
   var currentState = store.getState();
   console.log('currentState', currentState);
-  //unsubscribe();
+
+  fetchLocation();
 
   store.dispatch(changeName('Fredy'));
   store.dispatch(changeName('Emily'));
